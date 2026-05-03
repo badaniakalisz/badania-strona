6e66e2c2203902e87625e242360187507a2e0aef
abfipes12 <abfipes12@gmail.com>
2026-05-03 15:23:10 +0200

    fixing fixes + inline

---
+++ purgecss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['*.html', 'assets/js/*.js', 'assets/plugins/parallax/*'],
+  css: ['assets/css/theme.full.css'],
+  safelist: {
+    standard: [
+      'collapsing',
+    ],
+  },
+  output: 'assets/css/theme.notclean.css'
+}
